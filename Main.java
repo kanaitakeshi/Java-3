75b69238de9b5f2cbea808cb993fcf132a1cd819
Takeshi Kanai <101197022+kanaitakeshi@users.noreply.github.com>
2022-06-09 23:06:32 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+class Main {
+  public static void main(String[] args) {
+    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
+    Person person2 = new Person("John", "Christopher", "Smith", 65, 1.75, 80.0);
+
+    Car car = new Car("フェラーリ", "赤");
+    Bicycle bicycle = new Bicycle("ビアンキ", "緑");
+    
+    // buyメソッドを用いて、person1にcarを購入させる
+    person1.buy(car);
+    
+    // buyメソッドを用いて、person2にbicycleを購入させる
+    person2.buy(bicycle);
+
+    System.out.println("【車の情報】");
+    car.printData();
+    System.out.println("-----------------");
+    System.out.println("【車の所有者の情報】");
+    // getOwnerメソッドを用いてcarのownerを取得し、
+    // さらにprintDataメソッドを用いてownerの情報を出力する
+    car.getOwner().printData();
+
+
+    System.out.println("=================");
+    System.out.println("【自転車の情報】");
+    bicycle.printData();
+    System.out.println("-----------------");
+    System.out.println("【自転車の所有者の情報】");
+    // getOwnerメソッドを用いてbicycleのownerを取得し、
+    // さらにprintDataメソッドを用いてownerの情報を出力
+    bicycle.getOwner().printData();
+  }
+}
